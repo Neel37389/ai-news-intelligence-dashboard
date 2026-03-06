73d285fc15d51fccbd14c2c81be38c2484f3e6f8
patel <neelp060@gmail.com>
2026-03-05 23:02:29 -0500

    Added a badge in the ArticleItem so when the user savethe article the save badge appears

--- src/components/ArticleItem.jsx
+++ src/components/ArticleItem.jsx
@@ -7,17 +7,23 @@ import {
 } from "./ui/card";
 
 import { Button } from "./ui/button";
+import { Badge } from "./ui/badge";
 
 export const ArticleItem = ({ item, savedIds, toggleSave }) => {
   return (
     <Card className="transition hover:shadow-md h-full flex flex-col">
       <CardHeader>
-        <CardTitle>{item.title}</CardTitle>
+        <div className="flex items-center justify-between">
+          <CardTitle>{item.title}</CardTitle>
+          {savedIds.includes(item.id) && (
+            <Badge variant="secondary">Saved</Badge>
+          )}
+        </div>
         <CardDescription>{`Source: ${item.source}`}</CardDescription>
       </CardHeader>
       <CardFooter className="mt-auto">
         <Button onClick={() => toggleSave(item.id)}>
-          {savedIds.includes(item.id) ? "Saved" : "Save"}
+          {savedIds.includes(item.id) ? "Unsave" : "Save"}
         </Button>
       </CardFooter>
     </Card>
