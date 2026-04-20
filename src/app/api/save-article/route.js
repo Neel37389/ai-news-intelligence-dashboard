e52754d82f4a21a9a32241634b61b56a3101c094
patel <neelp060@gmail.com>
2026-04-20 18:11:36 -0400

    Added a delete method to delete one specific article or delete all the articles.

--- src/app/api/save-article/route.js
+++ src/app/api/save-article/route.js
@@ -30,3 +30,31 @@ export async function POST(req) {
     );
   }
 }
+
+export async function DELETE(req) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const article_id = searchParams.get("id");
+
+    if (article_id) {
+      const { error } = await supabase
+        .from("saved_articles")
+        .delete()
+        .eq("article_id", article_id);
+      if (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+      }
+      return NextResponse.json({ message: "Deleted One" });
+    }
+    const { error } = await supabase
+      .from("saved_articles")
+      .delete()
+      .neq("article_id", "");
+    if (error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+    return NextResponse.json({ message: "Deleted All" });
+  } catch (err) {
+    return NextResponse.json({ error: "Delete Failed" }, { status: 500 });
+  }
+}
